19ce6add335074fa679d746a00df477cab332b0e
dkp11 <dkp11@uw.edu>
2014-12-06 00:21:01 -0800

    Updated with pruned methods

--- PageRank.r
+++ PageRank.r
@@ -1,3 +1,7 @@
+###################################################
+######## Homework 2 - PageRank Algorithm  #########
+#########   Information Retrieval   ###############
+###################################################
 # Load the igraph library for processing 
 library(igraph)
 
@@ -26,6 +30,14 @@ n <- nrow(G)
 ###################################################
 ######## Method 1 begins here #####################
 ###################################################
+# We can either use the 'power' method or the 'eigen' method.
+# We will get negative values in eigen but ranking wont be affected.
+method <- 'power' 
+
+# we can either choose fixed iteration or use while loop for 
+# convergence. We are going for fixed iterations here as eigen
+# has convergence built in.
+iter <- 100
 
 # Preprocess/Normalize the data in the graph so that it can be used
 # for matrix and Eigen processing.
@@ -33,6 +45,7 @@ cvec <- apply(G,2,sum)
 
 # nodes with indegree 0 will cause problems if we divide by 0.
 cvec[cvec==0] <- 1 
+gvec <- apply(G,1,sum)
 
 # Calculate the delta and create the unity matrix same as G
 delta <- (1-d)/n
@@ -43,89 +56,32 @@ for (i in 1:n)
   A[i,] <- A[i,] + d*G[i,]/cvec
 
 # Get the Eigen vectors
-x <- Re(eigen(A)$vector[,1])
+if (method == 'power'){
+  x <- rep(1,n)
+  for (i in 1: iter) x <- A%*%x
+} else { # method == eigen
+  x <- Re(eigen(A)$vector[,1])
+}
+
+n = 1000
+#select the number of results to be displayed
+if( n > 100) n <- 100
 
 # Normalize, Sort and display the top 100 results.
-PRVect <- order(x/sum(x), decreasing = TRUE)[1:100]
+PRVect <- order(x/sum(x), decreasing = TRUE)[1:n]
 PRVect
-x[PRVect]
+abs(x[PRVect])
 ###################################################
 ######## Method 1 ends here #######################
 ###################################################
 
 ###################################################
-######## Method 2 begins here #####################
+######## Method 2 begins here #######################
+# This method should be used for verification purposes only
 ###################################################
 
-# Get G as a 2D matrix. 
-M <- as.matrix(G)
-
-# Get the node count
-n <- nrow(G)
-
-# Normalize matrix.
-M <- t(M / rowSums(M))
-
-# create a unity matrix of the same size.
-U <- matrix(data=rep(1/n, n^2), nrow=n, ncol=n)
-
-# Calculate the page rank.
-A <- d*M+(1-d)*U
-rm(M)
-rm(U)
-A[!is.finite(A)] <- 0
-v <- eigen(A)$vector[,1]
-
-# Normalize, Sort and display the top 100 results.
-PRVect <- order(as.numeric(v)/sum(as.numeric(v)), decreasing = TRUE)[1:100]
-PRVect
-v[PRVect]
+page.rank(inputGraph,directed = TRUE, damping = 0.85)
 
 ###################################################
-######## Method 2 ends here #######################
+######## Method 2 ends here ######################
 ###################################################
-
-###################################################
-######## Method 3 begins here #######################
-###################################################
-
-# This is the precision where convergence will take place.
-t = 20
-
-# normalize adjacency matrix by row sum and compute dangling node vector
-a = rep(0, n)
-
-# compute row sums (matrix multiplication)
-rs = G %*% rep(1,n)
-for (i in 1:n) {
-  if (rs[i] == 0) {a[i] = 1} 
-  else { G[i,] = G[i,] / rs[i] }  
-}
-
-# create a unity matrix of the same size.
-b <- as.matrix(rep(1/n, n))
-
-# Initialize values.
-e = rep(1, n)
-pi0 = rep(0, n)
-pi1 = rep(1/n, n)
-eps = 1/10^t
-iter = 0
-
-# Calculate page rank till convergence.
-while (sum(abs(pi0 - pi1)) > eps) {
-  pi0 = pi1
-  pi1 = d * pi1 %*% G + (d * pi1 %*% a + (1 - d) * pi1 %*% e) * b
-  iter = iter + 1
-} 
-
-# Return the normalized ist, sorted by top 100 page ranks
-pi2 = pi1 / sum(pi1)
-list(pi = pi2, iter = iter)
-PRVect <- (order(pi2, decreasing = TRUE))[1:100]
-PRVect
-
-###################################################
-######## Method 3  ends here ######################
-###################################################
-
